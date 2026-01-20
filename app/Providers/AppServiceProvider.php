c7b32fbe655b2f192e247f0745de3adf420d8120
ima-yusuke <yusuke1125aadxyz@gmail.com>
2026-01-20 21:09:55 +0900

    fix：httpsになるよう追記

--- app/Providers/AppServiceProvider.php
+++ app/Providers/AppServiceProvider.php
@@ -2,6 +2,7 @@
 
 namespace App\Providers;
 
+use Illuminate\Routing\UrlGenerator;
 use Illuminate\Support\Facades\Vite;
 use Illuminate\Support\ServiceProvider;
 
@@ -12,14 +13,14 @@ class AppServiceProvider extends ServiceProvider
      */
     public function register(): void
     {
-        //
+
     }
 
     /**
      * Bootstrap any application services.
      */
-    public function boot(): void
+    public function boot(UrlGenerator $url): void
     {
-        Vite::prefetch(concurrency: 3);
+        $url->forceScheme('https');
     }
 }
